e13e11ad86e60e36e9e5a4761b56c13c9444cab9
Nada Nabil <nn3122393@gmail.com>
2025-10-30 21:25:45 +0300

    Update task1_2.ino

--- task1_2.ino
+++ task1_2.ino
@@ -1,38 +1,40 @@
-
-//int button1 =2; 
-//int button2 =3; 
-//int button3 =4; 
+int LED1 = 5;
+int LED2 = 7;
+int LED3 = 9;
+int button1 =2; 
+int button2 =3; 
+int button3 =4; 
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(5, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-  pinMode(4, INPUT_PULLUP);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(button1, INPUT_PULLUP);
+  pinMode(button2, INPUT_PULLUP);
+  pinMode(button3, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int button_state1= digitalRead(2);
-  int button_state2= digitalRead(3);
-  int button_state3= digitalRead(4);
+  int button_state1= digitalRead(button1);
+  int button_state2= digitalRead(button2);
+  int button_state3= digitalRead(button3);
   if (button_state1 == LOW) {
-    digitalWrite(5, HIGH);
+    digitalWrite(LED1, HIGH);
   } else {
-    digitalWrite(5, LOW);
+    digitalWrite(LED1, LOW);
   }
 
   if (button_state2 == LOW) {
-    digitalWrite(7, HIGH);
+    digitalWrite(LED2, HIGH);
   } else {
-    digitalWrite(7, LOW);
+    digitalWrite(LED2, LOW);
   }
 
   if (button_state3 == LOW) {
-    digitalWrite(9, HIGH);
+    digitalWrite(LED3, HIGH);
   } else {
-    digitalWrite(9, LOW);
+    digitalWrite(LED3, LOW);
   }
 }
