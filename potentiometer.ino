b177c011fa25fd62a21a81c664b14af35a3d6cfb
Nada Nabil <nn3122393@gmail.com>
2025-11-18 23:50:43 +0200

    Add files via upload

--- potentiometer.ino
+++ potentiometer.ino
@@ -3,9 +3,8 @@ int LED1 = 3;
 int LED2 = 5;
 int LED3 = 6;
 int POT_read;
-int LED_1;
-int LED_2;
-int LED_3;
+int LED_;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(pot,INPUT);
@@ -18,14 +17,23 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   POT_read = analogRead(pot);
-  LED_1 =map(POT_read,0,1023,0,341);
-  analogWrite(LED1,LED_1);
-  Serial.println(LED_1);
-  LED_2 =map(POT_read,0,1023,342,683);
-  analogWrite(LED2,LED_2);
-  Serial.println(LED_2);
-  LED_3 =map(POT_read,0,1023,684,1023);
-  analogWrite(LED3,LED_3);
-  Serial.println(LED_3);
+  // LED_ =map(POT_read,0,1023,0,255);
+  if(POT_read >= 0 && POT_read <= 341){
+    analogWrite(LED1,POT_read);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,LOW);
+  }
+  else if(POT_read >= 342 && POT_read <= 683){
+    analogWrite(LED2,POT_read);
+    digitalWrite(LED1,LOW);
+    digitalWrite(LED3,LOW);
+  }
+  else if(POT_read >= 684 && POT_read <= 1023){
+    analogWrite(LED3,POT_read);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED1,LOW);
+    
+  }
+  Serial.println(LED_);
 }
       
